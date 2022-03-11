10ebab7862d85115c564a70efcdb46c181a29da5
planimir77 <planimir7708@gmail.com>
2022-03-11 13:39:06 +0200

    feat: add home module

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule} from '../app-routing.module';
+import { HomeComponent } from './home.component';
+
+
+@NgModule({
+  declarations: [
+  
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+  ]
+})
+export class HomeModule { }

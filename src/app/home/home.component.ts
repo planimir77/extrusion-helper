bade3b16c7ea53168ccc2af8fb68e48b0c3b09b9
planimir77 <planimir7708@gmail.com>
2022-03-23 21:46:39 +0200

    detect device screen width

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+    screenWidth: number | undefined;
 
-  constructor() { }
+  constructor() { 
+    this.getScreenSize();
+  }
 
   ngOnInit(): void {
   }
 
+  @HostListener('window:resize', ['$event'])
+    getScreenSize() {
+          this.screenWidth = window.innerWidth;
+    }
 }

2c9bf627917efd9a1d7c13a902478f3e01e5ad80
planimir77 <planimir7708@gmail.com>
2022-03-25 15:46:09 +0200

    style: dropdown position

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
+import {NgbDropdown} from '@ng-bootstrap/ng-bootstrap'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'extrusion-helper';
+  title = 'Extrusion Helper';
+
+  @ViewChild('myDrop') myDrop!: NgbDropdown;
+
+  @HostListener('window:scroll') onScroll(e: Event): void {
+    this.myDrop.close();
+  }
 }

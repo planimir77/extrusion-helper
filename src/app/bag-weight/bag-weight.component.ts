a1ee7f2e2eb43be5b7d4390563f4c21e314e47d1
planimir77 <planimir7708@gmail.com>
2022-04-30 17:49:43 +0300

    update: bag-weight component

--- src/app/bag-weight/bag-weight.component.ts
+++ src/app/bag-weight/bag-weight.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs';
+import { IDensityOption, IRange } from '../shared/interfaces';
 
 @Component({
   selector: 'app-bag-weight',
@@ -7,9 +10,122 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BagWeightComponent implements OnInit {
 
-  constructor() { }
+  calcForm!: FormGroup;
+  @ViewChild('weightUnit') weightUnit: any;
+  @ViewChild('quantityUnit') quantityUnit: any;
 
-  ngOnInit(): void {
+  spoolVolume!: number;
+  spoolRadius!: number;
+
+  selectOptionLocalValue!: string | null;
+  customOptionLocalValue!: string | null;
+
+  densityRange!: IRange;
+  defaultRange!: IRange;
+  thicknessRange!: IRange;
+  quantityRange!: IRange;
+
+  densityOptions: IDensityOption[] = [
+    { key: 'LDPE Regranulate', value: '0.88', },
+    { key: 'LDPE', value: '0.92', },
+    { key: 'LLDPE', value: '0.915', },
+    { key: 'HDPE', value: '0.941', },
+    { key: 'PP', value: '0.946', },
+  ];
+
+  constructor(private fb: FormBuilder) {
+    this.selectOptionLocalValue = localStorage.getItem('Selected');
+    this.customOptionLocalValue = localStorage.getItem('Custom');
+    this.densityRange = { min: 0.0898, max: 22.570 };
+    this.defaultRange = { min: 1, max: 1000 };
+    this.thicknessRange = { min: 1, max: 2000 };
+    this.quantityRange = { min: 1, max: 10000000 };
   }
 
+  ngOnInit(): void {
+    this.calcForm = this.fb.group({
+
+      densitySelect: [''],
+      densityInput: ['', [
+        Validators.required,
+        Validators.min(this.densityRange.min),
+        Validators.max(this.densityRange.max),
+        Validators.pattern("[0-9]*\.?[0-9]*$")]],
+      width: ['', [
+        Validators.required,
+        Validators.min(this.defaultRange.min),
+        Validators.max(this.defaultRange.max),
+        Validators.pattern("[0-9]*\.?[0-9]*$")]],
+      length: ['', [
+        Validators.required,
+        Validators.min(this.defaultRange.min),
+        Validators.max(this.defaultRange.max),
+        Validators.pattern("[0-9]*\.?[0-9]*$")]],
+      thickness: ['', [
+        Validators.required,
+        Validators.min(this.thicknessRange.min),
+        Validators.max(this.thicknessRange.max),
+        Validators.pattern("[1-9][0-9]*$")]],
+      quantity: ['1', [
+        Validators.required,
+        Validators.min(this.quantityRange.min),
+        Validators.max(this.quantityRange.max),
+        Validators.pattern("[0-9]*\.?[0-9]*$")]],
+      weight: [''],
+    });
+    this.calcForm
+      .valueChanges
+      .pipe(debounceTime(400))
+      .subscribe(() => {
+        if (this.calcForm.valid) {
+          this.refreshWeight();
+        }
+      });
+    this.quantity
+      ?.valueChanges
+      .subscribe(value => {
+        this.quantityUnit.nativeElement.innerText = value == "1" ? "bag" : "bags";
+      });
+
+    if (localStorage.getItem('Density')) {
+      this.densityInput?.patchValue(Number(localStorage.getItem('Density')));
+    }
+    else {
+      this.setDefaultDensityValue();
+    }
+  }
+  setDefaultDensityValue() {
+    this.densityInput?.patchValue(Number(this.densityOptions[0].value));
+  }
+  refreshWeight() {
+    const weight = this.calculateWeight();
+    this.weightUnit.nativeElement.innerText = weight.unit;
+    this.weight?.patchValue(weight.value, { emitEvent: false });
+  }
+
+  //#region  getters
+  get densitySelect() { return this.calcForm.get('densitySelect'); }
+  get densityInput() { return this.calcForm.get('densityInput'); }
+  get width() { return this.calcForm.get('width'); }
+  get length() { return this.calcForm.get('length'); }
+  get thickness() { return this.calcForm.get('thickness'); }
+  get quantity() { return this.calcForm.get('quantity'); }
+  get weight() { return this.calcForm.get('weight'); }
+  //#endregion
+
+  lengthClicked(event: any) {
+
+  }
+  calculateWeight(): { unit: string; value: number } {
+    const widthInCm: number = this.width?.value / 100;
+    const lengthInCm: number = this.length?.value / 100;
+    const thickness: number = this.thickness?.value;
+    const density: number = this.densityInput?.value;
+    const quantity: number = this.quantity?.value;
+    let value = widthInCm * lengthInCm * density * thickness * 2 * quantity;
+    const unit = value >= 1000 ? 'kg' : 'gr';
+    value = value >= 1000 ? value / 1000 : value;
+    return { unit, value };
+  }
 }
+
--- src/app/bag-weight/bag-weight.module.ts
+++ src/app/bag-weight/bag-weight.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { BagWeightComponent } from './bag-weight.component';
 import { BagWeightRoutingModule } from './bag-weight-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 
 
@@ -11,7 +15,10 @@ import { BagWeightRoutingModule } from './bag-weight-routing.module';
   ],
   imports: [
     CommonModule,
-    BagWeightRoutingModule
+    ReactiveFormsModule,
+    BagWeightRoutingModule,
+    SharedModule,
+    NgbModule
   ],
   exports: [
     BagWeightComponent

c620719a7051c0f3a166f005a3e2f0fe69df7e92
planimir77 <planimir7708@gmail.com>
2022-06-30 20:51:12 +0300

    feat: create progressive web app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeModule } from './home/home.module';
 import { RollWeightModule } from './roll-weight/roll-weight.module';
 import { BagWeightModule } from './bag-weight/bag-weight.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -19,6 +21,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     RollWeightModule,
     BagWeightModule,
     NgbModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]

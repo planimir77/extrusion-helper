60deb05e7c768988499615baacaf578d7f7c6de6
planimir77 <planimir7708@gmail.com>
2023-02-23 20:50:34 +0200

    feat: add custom width

--- src/app/roll-weight/roll-weight.component.ts
+++ src/app/roll-weight/roll-weight.component.ts
@@ -24,6 +24,8 @@ export class RollWeightComponent implements OnInit {
 
   densityOptions: IDensityOption[];
 
+  widthValue: number;
+
   constructor(private fb: FormBuilder, service: DensityOptionsService) {
 
     this.densityOptions = service.getOptions();
@@ -31,6 +33,7 @@ export class RollWeightComponent implements OnInit {
     this.customOptionLocalValue = localStorage.getItem('Custom');
     this.densityRange = { min: 0.0898, max: 22.570 };
     this.defaultRange = { min: 1, max: 1000 };
+    this.widthValue = 80;
   }
 
   ngOnInit(): void {
@@ -47,11 +50,13 @@ export class RollWeightComponent implements OnInit {
         Validators.min(this.densityRange.min),
         Validators.max(this.densityRange.max),
         Validators.pattern("[0-9]*\.?[0-9]*$")]],
-      weight: ['', [
+      width: ['', [
         Validators.required,
         Validators.min(this.defaultRange.min),
         Validators.max(this.defaultRange.max),
-        Validators.pattern("[0-9]*\.?[0-9]*$")]],
+        Validators.pattern(/^([0-9]*\.?[0-9]|[0-9]*\.?[0-9]\+[0-9]|[0-9]*\.?[0-9]\+[0-9]\*[0-9]*\.?[0-9])$/),
+        //Validators.pattern("[0-9]*\.?[0-9]*$")
+      ]],
       rollR: ['', [
         Validators.required,
         Validators.min(this.defaultRange.min),
@@ -77,33 +82,44 @@ export class RollWeightComponent implements OnInit {
   //#region  getters
   get spoolDiameter() { return this.calcForm.get('spoolDiameter'); }
   get densityInput() { return this.calcForm.get('densityInput'); }
-  get weight() { return this.calcForm.get('weight'); }
+  get width() { return this.calcForm.get('width'); }
   get rollR() { return this.calcForm.get('rollR'); }
   get result() { return this.calcForm.get('result'); }
   get densitySelect() { return this.calcForm.get('densitySelect'); }
   //#endregion
   onSubmit(form: FormGroup) {
-    throw new Error('Not implemented.');
-  }
-  rollRClicked(event: Event) {
-    throw new Error('Not implemented.');
-    // const visibility = event.currentTarget.childNodes[1].style.visibility;
-    // if (visibility != 'visible') {
-    //   event.currentTarget.childNodes[1].style.visibility ='visible';
-    //   let target = event.currentTarget.childNodes[1];
-    //   // setTimeout((target: any) => {
-    //   //   target.style.visibility ='hidden';
-    //   // }, 1000);
-    //}
+    //throw new Error('Not implemented.');
   }
   calculateResult(): void {
-    this.spoolRadius = this.spoolDiameter?.value / 2;
-    const rollVolume = Math.PI * Math.pow(this.spoolRadius + this.rollR?.value, 2) * this.weight?.value;
-    this.spoolVolume = Math.PI * Math.pow(this.spoolRadius, 2) * this.weight?.value;
-    const value = ((rollVolume - this.spoolVolume) * this.densityInput?.value) / 1000;
-    this.result?.patchValue(value, { emitEvent: false });
+    const calculatedWidth = this.calculateWidth();
+    if (!Number.isNaN(calculatedWidth)) {
+      this.spoolRadius = this.spoolDiameter?.value / 2;
+      const rollVolume = Math.PI * Math.pow(this.spoolRadius + this.rollR?.value, 2) * calculatedWidth;
+      this.spoolVolume = Math.PI * Math.pow(this.spoolRadius, 2) * calculatedWidth;
+      const value = ((rollVolume - this.spoolVolume) * this.densityInput?.value) / 1000;
+      this.result?.patchValue(value.toFixed(3), { emitEvent: false });
+    }
+  }
+  calculateWidth():number {
+    const arr = this.width?.value.split('+').filter((item: string) => item != "");
+    if (arr.length === 1) {
+      return Number(arr[0]);
+    }
+    else if (arr.length === 2) {
+      const secondExpression = arr[1].split('*').filter((item: string) => item != "");
+      return secondExpression.length === 1 
+                        ? (Number(arr[0]) + Number(secondExpression[0]))/2
+                        : (Number(arr[0]) + (Number(secondExpression[0]) * Number(secondExpression[1])))/2;
+    }
+    return NaN;
+
   }
   setDefaultDensityValue() {
     this.densityInput?.patchValue(Number(this.densityOptions[0].value));
+  }
+  onKeyUp(event: any) {
+    this.widthValue = event.target.value.length > 6 
+    ? 80 + (9 * (event.target.value.length - 6))
+    : 80;
   }
 }
--- src/app/roll-weight/roll-weight.module.ts
+++ src/app/roll-weight/roll-weight.module.ts
@@ -10,7 +10,7 @@ import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
-    RollWeightComponent
+    RollWeightComponent,
   ],
   imports: [
     CommonModule,
